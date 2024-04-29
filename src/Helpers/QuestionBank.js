fc6e88f81a3b663bf4d120c7cca120f0bda08433
Manasa14-2002 <133627278+Manasa14-2002@users.noreply.github.com>
2024-04-29 07:12:42 +0100

    Add files via upload

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,30 @@
+export const Questions = [
+    {
+        prompt: "what is 1+3 ?",
+        optionA: "24",
+        optionB: "21",
+        optionC: "4",
+        optionD: "2",
+        answer: "C",
+    },
+
+    {
+        prompt: "which is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Twitch",
+        optionC: "Google",
+        optionD: "Robingood",
+        answer: "B",
+    },
+
+    {
+        prompt: "what is best tech youtube channel?",
+        optionA: "Pedrotech",
+        optionB: "dev ed",
+        optionC: "fireship",
+        optionD: "techwithTim",
+        answer: "A",
+    },
+
+
+];
